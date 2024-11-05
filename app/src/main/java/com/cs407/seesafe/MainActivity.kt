ff70eb2444470c9217022bbd2e4aacf3eb8f5c9a
Liron541 <wzy0430@qq.com>
2024-11-05 14:46:43 -0600

    some basic pages

--- app/src/main/java/com/cs407/seesafe/MainActivity.kt
+++ app/src/main/java/com/cs407/seesafe/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.cs407.seesafe
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val topButton: Button = findViewById(R.id.topButton)
+        topButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/cs407/seesafe/PageForHelper.kt
@@ -0,0 +1,16 @@
+package com.cs407.seesafe
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class PageForHelper : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        // 在这里添加任何登录页面的初始化代码
+    }
+}
